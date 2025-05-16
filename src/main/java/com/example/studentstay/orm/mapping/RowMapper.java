8e6e5ed79855a8372d195e49afba8c3966009d09
sinemoraa <jonshilin.gg@gmail.com>
2025-05-16 21:05:34 +0300

    feat: add RowMapper interface for mapping rows from ResultSet

---
+++ src/main/java/com/example/studentstay/orm/mapping/RowMapper.java
@@ -0,0 +1,9 @@
+package com.example.studentstay.orm.mapping;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+    T mapRow(ResultSet rs) throws SQLException;
+}
